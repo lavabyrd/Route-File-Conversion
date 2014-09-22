cf3eb5503a06841b012f1319fcf8ac3e3d649f40
splinterific <markgpreston@gmail.com>
2014-09-22 15:50:44 +0100

    added Script

---
+++ routeConvert.py
@@ -0,0 +1,34 @@
+import csv
+
+
+with open("rca original.txt", 'rb') as i:
+	reader = csv.reader(i, delimiter = '|', quoting = csv.QUOTE_MINIMAL)
+	with open("fixed.txt", 'wb') as o:
+		writer = csv.writer(o, delimiter = '|', quoting = csv.QUOTE_MINIMAL)
+		for row in reader:
+			if row[8] == "SX01":
+				row[8] = "GW05"
+				row[9] = "GW05"
+				writer.writerow(row)
+			elif row[8] == "SX02":
+				row[8] = "GW06"
+				row[9] = "GW06"
+				writer.writerow(row)
+			elif row[8] == "SX03":
+				row[8] = "NWX1"
+				row[9] = "NWX1"
+				writer.writerow(row)
+			elif row[8] == "SX04":
+				row[8] = "NWX2"
+				row[9] = "NWX2"
+				writer.writerow(row)
+			elif row[8] == "SX05":
+				row[8] = "NWX3"
+				row[9] = "NWX3"
+				writer.writerow(row)
+			elif row[8] == "SX07":
+				row[8] = "NWX4"
+				row[9] = "NWX4"
+				writer.writerow(row)
+			else:
+				writer.writerow(row)
